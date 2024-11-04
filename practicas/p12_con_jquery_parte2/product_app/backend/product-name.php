2609691d475a970ce955f573defd158a040925b9
unknown <noe.lariosg@alumno.buap.mx>
2024-11-03 23:06:09 -0600

    product-name.php

---
+++ practicas/p12_con_jquery_parte2/product_app/backend/product-name.php
@@ -0,0 +1,15 @@
+<?php
+include_once __DIR__.'/database.php';
+
+$response = ['exists' => false];
+if (isset($_GET['name'])) {
+    $name = $conexion->real_escape_string($_GET['name']);
+    $result = $conexion->query("SELECT id FROM productos WHERE nombre = '$name' AND eliminado = 0");
+
+    if ($result && $result->num_rows > 0) {
+        $response['exists'] = true;
+    }
+}
+
+echo json_encode($response);
+?>
