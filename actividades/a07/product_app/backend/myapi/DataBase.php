9505445780fca61c17a9e906122d73531ac5335c
unknown <noe.lariosg@alumno.buap.mx>
2024-11-07 18:49:03 -0600

    Se crea DataBase

---
+++ actividades/a07/product_app/backend/myapi/DataBase.php
@@ -0,0 +1,20 @@
+<?php
+
+//namespace TECWEB1/MYAPI
+
+abstract class DataBase {
+    protected $conexion = NULL;
+    
+    public function __construct($user,$pass,$db) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+        if (!$this->conexion) {
+            die('Error de conexión (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+        }
+    }
+}
+?>
